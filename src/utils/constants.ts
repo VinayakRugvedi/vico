dcb93f9ca4ddca24a3623d26663b5e79e57dba77
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2024-03-16 12:40:36 +0530

    chore: create utils dir and add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+const ASPECT_RATIOS = {
+  Large: {
+    width: 16,
+    height: 9,
+  },
+  Medium: {
+    width: 4,
+    height: 3,
+  },
+  Square: {
+    width: 1,
+    height: 1,
+  },
+};
+
+export { ASPECT_RATIOS };
