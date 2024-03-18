f41a5e9bd257ecd93a9535ff1d8a639d794fedd8
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2024-03-18 13:06:12 +0530

    chore: create sidepanel component

---
+++ src/pages/Meeting/components/sidepanel.tsx
@@ -0,0 +1,86 @@
+import { X, Wand } from "lucide-react";
+
+import { Button } from "components";
+import { Sheet, SheetContent } from "components";
+
+import { cn } from "utils/lib";
+
+interface PropTypes {
+  showSidePanel: boolean;
+  hallHeight: number;
+  hallWidth: number;
+  handleSidePanelState: () => void;
+}
+
+function SidePanel({
+  showSidePanel,
+  hallHeight,
+  hallWidth,
+  handleSidePanelState,
+}: PropTypes) {
+  if (hallWidth >= 750) {
+    return (
+      <div
+        className={cn(
+          "bg-secondary rounded-lg self-center transition-[flex-basis] duration-700 ease-out",
+          !showSidePanel ? "basis-0" : "basis-[450px]",
+        )}
+        style={{
+          height: `${hallHeight - 16}px`,
+        }}
+      >
+        {showSidePanel && (
+          <div className="h-full">
+            <div className="w-full flex items-center justify-between p-4 border-b border-primary/30">
+              <h6 className="text-lg font-medium">Side Panel</h6>
+              <Button
+                className="rounded-full h-10 w-10"
+                onClick={() => handleSidePanelState()}
+              >
+                <span>
+                  <X className="h-4 w-4" />
+                </span>
+              </Button>
+            </div>
+            <div className="w-full flex items-center justify-center flex-col gap-8 h-[70%]">
+              <span>
+                <Wand className="w-20 h-20 text-primary" />
+              </span>
+              <p className="text-center">More actions will appear here.</p>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <Sheet open={showSidePanel} onOpenChange={handleSidePanelState}>
+      <SheetContent className="dark p-0 bg-secondary text-foreground w-[325px]">
+        <div className="w-full flex items-center justify-between p-4 border-b border-primary/30">
+          <h6 className="text-lg font-medium">Side Panel</h6>
+          <Button
+            className="rounded-full h-10 w-10"
+            onClick={() => handleSidePanelState()}
+          >
+            <span>
+              <X className="h-4 w-4" />
+            </span>
+          </Button>
+        </div>
+
+        <div className="h-full">
+          <div className="px-4 flex items-center justify-center flex-col gap-8 h-[70%]">
+            <span>
+              <Wand className="w-20 h-20 text-primary" />
+            </span>
+
+            <p className="text-center">More actions will appear here.</p>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default SidePanel;
