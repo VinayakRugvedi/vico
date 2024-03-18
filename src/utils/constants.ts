b035eecb5313ae62f2150b573da3c95e646e68e4
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2024-03-18 12:36:03 +0530

    chore: add limit and max constant, minor update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,15 +2,45 @@ const ASPECT_RATIOS = {
   Large: {
     width: 16,
     height: 9,
+    display: "16 : 9",
   },
   Medium: {
     width: 4,
     height: 3,
+    display: "4 : 3",
   },
   Square: {
     width: 1,
     height: 1,
+    display: "1 : 1",
   },
 };
 
-export { ASPECT_RATIOS };
+export type AspectRatioNames = "Large" | "Medium" | "Square";
+
+const HALL_PARTICIPANTS_LIMIT = [
+  {
+    minWidth: 1280,
+    maxVisibleParticipants: 49,
+  },
+  {
+    minWidth: 1024,
+    maxVisibleParticipants: 34,
+  },
+  {
+    minWidth: 768,
+    maxVisibleParticipants: 29,
+  },
+  {
+    minWidth: 640,
+    maxVisibleParticipants: 15,
+  },
+  {
+    minWidth: 300,
+    maxVisibleParticipants: 10,
+  },
+];
+
+const MAX_PARTICIPANTS = 10000;
+
+export { ASPECT_RATIOS, HALL_PARTICIPANTS_LIMIT, MAX_PARTICIPANTS };
